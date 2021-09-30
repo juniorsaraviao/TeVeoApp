8325657226946364c172df7fb701410b8bc8660a
juniorsaraviao <junior.saravia.o@uni.pe>
2021-09-29 22:35:02 -0500

    Agregar archivos de proyecto.

---
+++ TeVeo/TeVeo/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using TeVeo.ViewModels;
+using TeVeo.Views;
+using Xamarin.Forms;
+
+namespace TeVeo
+{
+   public partial class AppShell : Xamarin.Forms.Shell
+   {
+      public AppShell()
+      {
+         InitializeComponent();
+         Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+         Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+      }
+
+      private async void OnMenuItemClicked(object sender, EventArgs e)
+      {
+         await Shell.Current.GoToAsync("//LoginPage");
+      }
+   }
+}
---
+++ TeVeo/TeVeo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TeVeo.ViewModels;
+using Xamarin.Forms;
+
+namespace TeVeo.Views
+{
+   public partial class ItemDetailPage : ContentPage
+   {
+      public ItemDetailPage()
+      {
+         InitializeComponent();
+         BindingContext = new ItemDetailViewModel();
+      }
+   }
+}
